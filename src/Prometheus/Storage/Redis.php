8a125b71b9324f9823cbf68585ac2552584bf141
RobinPeng <robin.peng@qq.com>
2017-04-19 14:34:01 +0800

    Update Redis.php

--- src/Prometheus/Storage/Redis.php
+++ src/Prometheus/Storage/Redis.php
@@ -92,6 +92,12 @@ class Redis implements Adapter
                 @$this->redis->connect($this->options['host'], $this->options['port'], $this->options['timeout']);
             }
             $this->redis->setOption(\Redis::OPT_READ_TIMEOUT, $this->options['read_timeout']);
+            if (isset($this->options['password'])) {
+              @$this->redis->auth($this->options['password']);
+            }
+            if (isset($this->options['database'])) {
+              @$this->redis->select($this->options['database']);
+            }
         } catch (\RedisException $e) {
             throw new StorageException("Can't connect to Redis server", 0, $e);
         }
